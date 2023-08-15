546e408fb4b78af303057dfb60c6f975ca82046a
Fernanda Gonzalez <fernandagch@ciencias.unam.mx>
2023-08-14 21:25:11 -0600

    Session 7: 80-98 Gifs App

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { SearchResponse, Gif } from '../interfaces/gifs.interface';
+
+@Injectable({providedIn: 'root'})
+export class GifsService {
+
+  public gifList: Gif[] = [];
+
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'Xc3Pi4bwdy5CvHxbizGML9JUEUlMl9e4';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) {
+    this.loadLocalStorage();
+  }
+
+  get tagsHistory(): string[] {
+    return [...this._tagsHistory];
+  }
+
+  private organizeHistory( tag: string ): void {
+    tag = tag.toLowerCase();
+
+    if ( this._tagsHistory.includes(tag) ) {
+      this._tagsHistory = this._tagsHistory.filter( oldTag => oldTag !== tag );
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! );
+
+    if ( this._tagsHistory.length > 0 ) {
+      this.searchTag(this._tagsHistory[0]);
+    }
+
+  }
+
+  searchTag( tag: string ): void {
+    if (tag.length === 0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', 10)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe( resp => {
+        this.gifList = resp.data;
+      });
+
+  }
+}
